e7390461823bff5b45374816391032b0adf6734c
Amalie <Amal956e@stud.kea.dk>
2022-09-28 16:15:44 +0200

    1. udgave er færdig

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 const button = document.querySelector("button");
 console.log(button);
 button.addEventListener('click', function () {
-    const number =
-    console.log("tal")
+
+    let input1 = parseInt(document.querySelector("#tal1").value);
+    let input2 = parseInt(document.querySelector("#tal2").value);
+    let op = document.querySelector("#operator").value;
+    console.log(input1 + " " + input2 + " " + op);
+    let calculate;
+
+    if (op == "+") {
+        calculate = input1 + input2;
+    } else if (op == "-") {
+        calculate = input1 - input2;
+    } else if (op == "*") {
+        calculate = input1 * input2;
+    } else if (op == "/") {
+        calculate = input1 / input2;
+    }
+    document.querySelector("#result").innerHTML = calculate;
 });
+/*
+function calc(operator) {
+    let a = parseInt(document.querySelector("#tal1").value);
+    let b = parseInt(document.querySelector("#tal2").value);
+    let op = document.querySelector("#operator").value;
+    console.log(a + " " + b + " " + op);
+    let calculate;
+
+    if (op == "+") {
+        calculate = a + b;
+    } else if (op == "-") {
+        calculate = a - b;
+    }else if (op == "*") {
+        calculate = a * b;
+    } else if (op == "/") {
+        calculate = a / b;
+    }
+    document.querySelector("#result").innerHTML = calculate;
+}
+*/
