e08a935b46373a9588c8fd3c46d776b7856b8f97
Amalie <Amal956e@stud.kea.dk>
2022-10-05 19:52:09 +0200

    Ændring 3

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const button = document.querySelector("#submit");
-console.log(button);
-
 button.addEventListener('click', function (event) {
     event.preventDefault();
 
     let input1 = parseFloat(document.querySelector("#tal1").value);
     let input2 = parseFloat(document.querySelector("#tal2").value);
-    /*
-        console.log("inputtet som en streng " + typeof input1.toString());
-        console.log("længden er som en streng " + input1.toString().length);
-        console.log(input1.toString() === "NaN");
-    */
-    //Beregn = intet skevet i inputene, log ud:
-    //if (submit.toString() === "NaN")
-
-
-    //Input1 = skrevet bogstav stedet for tal, log ud:
-    if (input1.toString() === "NaN") {
-        alert("Ups, du har skrevet forkert. Prøv igen");
-    }
-    //Input2 = skrevet bogstav stedet for tal, log ud:
-    else if (input2.toString() === "NaN") {
-        alert("Ups, du har skrevet forkert. Prøv igen");
-    }
-//Vælg imellem = nul valgte, log ud:
-
-
     let op = document.querySelector("#operator").value;
-    console.log(input1 + " " + input2 + " " + op);
+
+    if (input1.toString() === "NaN") {
+        let x = document.querySelector("#tal1").value;
+        if (x.trim() === "") {
+            alert("Ups, du har skrevet forkert. Prøv igen.");
+        } else {
+            alert("Ups, du har skrevet forkert. Prøv igen. Du skrev: " + x.trim() + " venligst skriv et tal");
+        }
+        return;
+    }
+
+    if (input2.toString() === "NaN") {
+        alert("Ups, du har skrevet forkert. Prøv igen");
+        return;
+    }
+
+    if (op === "") {
+        alert("Indtast en operator");
+        return;
+    }
+
     let calculate;
 
     if (op == "+") {
