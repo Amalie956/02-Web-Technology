33605affb16b2ddf3f2e63d42261b2d9d34ee64c
Amalie <Amal956e@stud.kea.dk>
2022-10-07 16:30:23 +0200

    Ændring 6 + readonly i html

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const button = document.querySelector("#submit");
 button.addEventListener('click', function (event) {
-    event.preventDefault();
+        event.preventDefault();
 
-    let input1 = parseFloat(document.querySelector("#tal1").value);
-    let input2 = parseFloat(document.querySelector("#tal2").value);
-    let op = document.querySelector("#operator").value;
+        let input1 = parseFloat(document.querySelector("#tal1").value);
+        let input2 = parseFloat(document.querySelector("#tal2").value);
+        let op = document.querySelector("#operator").value;
 
-    if (input1.toString() === "NaN") {
-        let x = document.querySelector("#tal1").value;
-        if (x.trim() === "") {
-            alert("Ups, du har skrevet forkert i boksen. Prøv igen.");
-        } else {
-            alert("Ups, du har skrevet forkert. Du skrev: " + x.trim() + " venligst skriv et tal");
+        if (input1.toString() === "NaN") {
+            let x = document.querySelector("#tal1").value;
+            if (x.trim() === "") {
+                alert("Ups, du har skrevet forkert i boksen. Prøv igen.");
+            } else {
+                alert("Ups, du har skrevet forkert. Du skrev: " + x.trim() + " venligst skriv et tal");
+            }
+            return;
         }
-        return;
-    }
 
-    if (input2.toString() === "NaN") {
-        let y = document.querySelector("#tal2").value;
-        if (y.trim() === "") {
-            alert("Ups, du har skrevet forkert i boksen. Prøv igen");
-        } else {
-            alert("Ups, du har skrevet forkert. Du skrev: " + y.trim() + " venligst skriv et tal");
+        if (input2.toString() === "NaN") {
+            let y = document.querySelector("#tal2").value;
+            if (y.trim() === "") {
+                alert("Ups, du har skrevet forkert i boksen. Prøv igen");
+            } else {
+                alert("Ups, du har skrevet forkert. Du skrev: " + y.trim() + " venligst skriv et tal");
+            }
+            return;
         }
-        return;
-    }
         if (op === "") {
-            alert("Du har glemt at indtaste en perator. Vælg venligst en af nedestående:");
+            alert("Du har glemt at indtaste en operator. Vælg venligst en af nedestående:");
             return;
         }
 
@@ -41,12 +41,7 @@ button.addEventListener('click', function (event) {
         } else if (op == "/") {
             calculate = input1 / input2;
         }
-        const p = document.querySelector('p');
-        p.innerHTML = "Resultat er";
 
-        document.querySelector("#result").innerHTML = calculate;
+        document.querySelector("#finalResult").value = calculate;
     }
 )
-    ;
-
-
